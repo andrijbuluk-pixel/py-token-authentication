00d3dd9581f3c95452b932b8174e75403539127a
Andriy <andrijbuluk@gmail.com>
2026-06-22 08:27:18 +0200

    Aded: views and urls for login

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,13 @@
 from rest_framework.urls import path
-from user.views import CreateUser
 
+from user.views import (
+    CreateUser,
+    CreateTokenView
+)
 
 urlpatterns = [
     path("register/", CreateUser.as_view(), name="create_user"),
+    path("login/", CreateTokenView.as_view(), name="obtain_pass"),
 ]
 
 app_name = "user"
