98341a82bb6d00acee7f3c8f30d519275d71dbb5
Andriy <andrijbuluk@gmail.com>
2026-06-22 10:41:17 +0200

    Fix: model Ticket in def save

--- cinema/models.py
+++ cinema/models.py
@@ -95,9 +95,9 @@ class Ticket(models.Model):
                 raise error_to_raise(
                     {
                         ticket_attr_name: f"{ticket_attr_name} number "
-                        f"must be in available range: "
-                        f"(1, {cinema_hall_attr_name}): "
-                        f"(1, {count_attrs})"
+                                          f"must be in available range: "
+                                          f"(1, {cinema_hall_attr_name}): "
+                                          f"(1, {count_attrs})"
                     }
                 )
 
@@ -110,15 +110,17 @@ class Ticket(models.Model):
         )
 
     def save(
-        self,
-        force_insert=False,
-        force_update=False,
-        using=None,
-        update_fields=None,
+            self,
+            force_insert=False,
+            force_update=False,
+            using=None,
+            update_fields=None,
+            *args,
+            **kwargs,
     ):
         self.full_clean()
         return super(Ticket, self).save(
-            force_insert, force_update, using, update_fields
+            *args, **kwargs
         )
 
     def __str__(self):
