cf3af83bd4c77e2a40c272bca3668b07f0590e2b
Andriy <andrijbuluk@gmail.com>
2026-06-22 08:06:01 +0200

    Aded: urls on register

--- user/urls.py
+++ user/urls.py
@@ -1,1 +1,9 @@
-# write your code here
+from rest_framework.urls import path
+from user.views import CreateUser
+
+
+urlpatterns = [
+    path("register/", CreateUser.as_view(), name="create_user"),
+]
+
+app_name = "user"
